d8e9c2a6be8c2a4d5a9d6316d6230d02203c5063
Grzegorz Kućmierz <gkucmierz@gmail.com>
2017-02-10 12:06:48 +0100

    use scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { MaterialModule } from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
