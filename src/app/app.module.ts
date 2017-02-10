66e26df537e7ace1acd88ec8529b145b2d980147
Grzegorz Kućmierz <gkucmierz@gmail.com>
2017-02-10 19:14:56 +0100

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MaterialModule } from '@angular/material';
 
@@ -9,15 +10,32 @@ import { AppComponent } from './app.component';
 
 import 'hammerjs';
 
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export const routeConfig:Routes = [
+  {
+      path: '',
+      component: HomeComponent
+  },
+  {
+      path: '**',
+      component: NotFoundComponent
+  }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(routeConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
