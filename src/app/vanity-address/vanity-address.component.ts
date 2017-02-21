7654b33b3a7a3928772a9fd5e0f510ccd46368f3
Grzegorz Kućmierz <gkucmierz@gmail.com>
2017-02-21 07:21:17 +0100

    vanity-address: basic version

--- src/app/vanity-address/vanity-address.component.ts
+++ src/app/vanity-address/vanity-address.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ApplicationRef } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { SET_TITLE } from '../reducers/toolbarReducer';
@@ -8,12 +8,77 @@ import { SET_TITLE } from '../reducers/toolbarReducer';
   templateUrl: './vanity-address.component.html',
   styleUrls: ['./vanity-address.component.scss']
 })
-export class VanityAddressComponent implements OnInit {
+export class VanityAddressComponent implements OnInit, OnDestroy {
+  private startsWith = '1';
+  private ignoreCase = false;
+  private limit = 20;
+  private found = [];
+  private workers = [];
+  private threads = 1;
+  private addressCnt = 0;
+  private cores;
 
-  constructor(private store: Store<any>) { }
+  constructor(private store: Store<any>,
+              private appRef: ApplicationRef) {
+    this.cores = Array(navigator['hardwareConcurrency'] || 8).fill(0).map((a,i) => i+1);
+  }
+
+  spawnWorker(startsWith, ignoreCase) {
+    let worker = new Worker('/assets/vanity-address.worker.js');
+
+    worker.onmessage = msg => {
+      this.workerMessage(JSON.parse(msg.data));
+    };
+    worker.postMessage(JSON.stringify({
+      startsWith, ignoreCase
+    }));
+
+    this.workers.push(worker);
+  }
+
+  workerMessage(res) {
+    if (res.found) {
+      this.found.push({
+        address: res.address,
+        privkey: res.privkey
+      });
+      if (this.found.length >= this.limit) {
+        this.terminateWorkers();
+      }
+    }
+    this.addressCnt += res.cnt;
+    this.appRef.tick();
+  }
+
+  scale(force) {
+    let start = force || this.workers.length > 0;
+    this.terminateWorkers();
+    if (start) {
+      while (this.workers.length < this.threads) {
+        this.spawnWorker(this.startsWith, this.ignoreCase);
+      }
+    }
+  }
+
+  terminateWorkers() {
+    this.workers.map(worker => worker.terminate());
+    this.workers = [];
+  }
+
+  generate() {
+    this.scale(true);
+  }
+
+  clean() {
+    this.found = [];
+    this.addressCnt = 0;
+  }
 
   ngOnInit() {
     this.store.dispatch({ type: SET_TITLE, payload: 'Vanity Address' });
   }
 
+  ngOnDestroy() {
+    this.terminateWorkers();
+  }
 }
